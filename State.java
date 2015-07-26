2f71f774bb048602db5ca39bf175b2f93406b7a0
Suganya Thurairatnam <kichaa4@gmail.com>
2015-07-26 10:59:46 +0530

    state interface

---
+++ State.java
@@ -0,0 +1,13 @@
+/****************State Interface******************************************/
+
+/**************************************************************************
+First of all we will create State interface that 
+will define the method that should be implemented 
+by different concrete states and context class.
+**************************************************************************/
+//package com.journaldev.design.state;
+public interface State {
+
+	public void doAction();
+}
+
